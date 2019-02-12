ba9cfad0f17fef8c611ec28a674f88003b6c4e6f
Bharath <bkumar@whispir.com>
2019-02-13 10:08:25 +1100

    aded a line

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -2,30 +2,31 @@
 import boto3
 import click
 
-session = boto3.Session(profile_name='bharath-profile')
+session = boto3.Session(profile_name='PythonAutomation')
 s3 = session.resource('s3')
 
 
 # print(sys.argv)
 @click.group()
 def cli():
-    "Webotron will be deploy into the AWS"
+        "Webotron will be deploy into the AWS"
+        pass
 
 @cli.command('list-buckets')
 #@click.command('list-buckets')
 def list_buckets():
-    "List all S3 buckets"
-    for bucket in s3.buckets.all():
-        print(bucket)
+        "List all S3 buckets"
+        for bucket in s3.buckets.all():
+                print(bucket)
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 
 def list_bucket_objects(bucket):
-    "List all the S3 bucket objects"
-    for obj in s3.Bucket(bucket).objects.all():
-        print (obj)
+        "List all the S3 bucket objects"
+        for obj in s3.Bucket(bucket).objects.all():
+                print (obj)
 
 if __name__ == "__main__":
-    cli()
-    #list_buckets()
+        cli()
+        #list_buckets()
