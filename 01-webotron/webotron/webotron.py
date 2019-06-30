e002c419e7bb835bd20149e8af28eaf087663013
Bharath Kumar <Bharath.Kumar@medibank.com.au>
2019-06-30 14:01:52 +1000

    s3 bucket changes

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,81 +1,30 @@
-#import sys
 import boto3
 import click
-from botocore.exceptions import ClientError
 
-session = boto3.Session(profile_name='PythonAutomation')
+session = boto3.Session(profile_name='bharath')
 s3 = session.resource('s3')
 
-
-# print(sys.argv)
 @click.group()
 def cli():
-        "Webotron will be deploy into the AWS"
+        "Webotron will be deployed in AWS"
         pass
 
-@cli.command('list-buckets')
-#@click.command('list-buckets')
+@cli.command("list-buckets")
 def list_buckets():
-        "List all S3 buckets"
+        "list of buckets"
         for bucket in s3.buckets.all():
                 print(bucket)
 
-@cli.command('list-bucket-objects')
-@click.argument('bucket')
+@click.argument("bucket")
 
+@cli.command("list-bucket-objects")
 def list_bucket_objects(bucket):
-        "List all the S3 bucket objects"
+        "list all the bucket objects"
         for obj in s3.Bucket(bucket).objects.all():
                 print (obj)
 
-@cli.command('setup-bucket')
-@click.argument('bucket')
-def setup_bucket(bucket):
-        "Create and configure S3 website"
-        s3_bucket = None
-
-        try:
-                s3_bucket = s3.create_bucket(
-                        Bucket=bucket, 
-                        CreateBucketConfiguration={'LocationConstraint': session.region_name}
-                        )
-        except ClientError as e:
-                if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
-                        s3_bucket = s3.Bucket(bucket)
-                else:
-                        raise e
-
-        policy = """
-        {
-        "Version":"2012-10-17",
-        "Statement":[{
-        "Sid":"PublicReadGetObject",
-        "Effect":"Allow",
-        "Principal": "*",
-                "Action":["s3:GetObject"],
-                "Resource":["arn:aws:s3:::%s/*"
-                ]
-                }
-        ]
-        }
-        """ % s3_bucket.name
-
-        policy = policy.strip()
-        
-        pol = s3_bucket.Policy()
-        pol.put(Policy=policy)
-
-        ws =s3_bucket.Website()
-        ws.put(WebsiteConfiguration={
-                'ErrorDocument': {
-                'Key': 'error.html'
-                },
-                'IndexDocument': {
-                'Suffix': 'index.html'
-                }
-        })
-        return
-
 if __name__ == "__main__":
         cli()
-        #list_buckets()
+
+
+
